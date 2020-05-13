ac7fe787ea62028766b34d43f8ec735930fcdf28
Eugene Luo <eyluo@andrew.cmu.edu>
2020-05-13 12:22:31 -0400

    added support for statements and assignments in AST

--- ocaml/ast.mli
+++ ocaml/ast.mli
@@ -1,5 +1,14 @@
 type exp = 
+    | Var of string
     | Operator of Lexer.op * exp * exp
     | IntVal of int
 
+type stmt = 
+    | Assign of exp * exp
+    | Return of exp
+
+type program = stmt list
+
 val string_of_exp : exp -> string
+val string_of_stmt : stmt -> string
+val string_of_program : program -> string
