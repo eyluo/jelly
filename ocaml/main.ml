10ec953b9b8970533c7569e172588f46515a1d40
Eugene Luo <eyluo@andrew.cmu.edu>
2020-05-13 11:55:02 -0400

    added tests

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -1,44 +1,57 @@
 exception TestFail of string
 
+module L = Lexer
+module P = Parser
+
 let () = 
     (* Prints all of the tokens. *)
     let rec print_tokens lxr = 
-        let tok = Lexer.pop lxr in
-        let () = print_string (Lexer.string_of_token tok) in
+        let tok = L.pop lxr in
+        let () = print_string (L.string_of_token tok) in
         match tok with
-        | Lexer.Eof -> ()
+        | L.Eof -> ()
         | _ -> (print_tokens lxr)
     in
 
     let rec test_peek lxr = 
-        let tok1 = Lexer.peek lxr in 
-        let tok2 = Lexer.pop lxr in
+        let tok1 = L.peek lxr in 
+        let tok2 = L.pop lxr in
         if tok1 <> tok2 then raise (TestFail "peek and pop returned different values!")
-        else if tok1 = Lexer.Eof then ()
+        else if tok1 = L.Eof then ()
         else test_peek lxr
     in 
     
-    let () = print_tokens (Lexer.create "../tests/add.test") in
+    let () = print_tokens (L.create "../tests/add.test") in
     let () = print_endline("") in
-    let () = print_tokens (Lexer.create "../tests/whitespace.test") in
+    let () = print_tokens (L.create "../tests/whitespace.test") in
     let () = print_endline("") in
-    let () = print_tokens (Lexer.create "../tests/bigboys.test") in
+    let () = print_tokens (L.create "../tests/bigboys.test") in
     let () = print_endline("") in
-    let () = print_tokens (Lexer.create "../tests/pemdas.test") in
+    let () = print_tokens (L.create "../tests/pemdas.test") in
     let () = print_endline("") in
-    let () = test_peek (Lexer.create "../tests/whitespace.test") in
+    let () = test_peek (L.create "../tests/whitespace.test") in
 
 
-    let ast = Parser.parse (Lexer.create "../tests/add.test") in
+    let ast = P.parse (L.create "../tests/add.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    let ast = Parser.parse (Lexer.create "../tests/whitespace.test") in
+    let ast = P.parse (L.create "../tests/whitespace.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    let ast = Parser.parse (Lexer.create "../tests/bigboys.test") in
+    let ast = P.parse (L.create "../tests/bigboys.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    let ast = Parser.parse (Lexer.create "../tests/precedence.test") in
+    let ast = P.parse (L.create "../tests/precedence.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    let ast = Parser.parse (Lexer.create "../tests/pemdas.test") in
+    let ast = P.parse (L.create "../tests/pemdas.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    let ast = Parser.parse (Lexer.create "../tests/pemdas2.test") in
+    let ast = P.parse (L.create "../tests/pemdas2.test") in
     let () = print_endline (Ast.string_of_exp ast) in
-    ()
+
+    let () = print_tokens (L.create "../tests/assignment.test") in
+    let () = print_endline("") in
+    let () = print_tokens (L.create "../tests/statements.test") in
+    let () = print_endline("") in
+    let () = print_tokens (L.create "../tests/abc.test") in
+    let () = print_endline("") in
+    try print_tokens (L.create "../tests/illegalvar.test")
+    with
+    | L.InvalidInt _ -> print_endline("caught illegal variable")
+    | _ -> assert false
